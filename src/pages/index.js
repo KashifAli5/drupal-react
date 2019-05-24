c2148031c44bbb5a909d0c301b6b793a63abe5e5
KashifAli5 <kashif.ali@devtrongenesis.com>
2019-05-24 14:22:03 +0500

    my first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,41 @@
 import React from "react"
 import { Link } from "gatsby"
+import { StaticQuery, graphql } from 'gatsby'
 
-import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <StaticQuery
+    query={graphql`
+        query {
+         allNodePage 
+        {
+          edges {
+            node {
+              id
+              title
+              body {
+                value
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+        <div>
+        {data.allNodePage.edges.map(({ node }, i) => (
+          <li key={i}> page title <h3> {node.title}</h3> <br /><br /> {node.body.value} </li>
+
+        ))}
+        
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    )}
+  />
 )
+
+
+
 
 export default IndexPage
